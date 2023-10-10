2be80e3d6b89a845d8d5cbcf89806bbb226777b3
gokkuu100 <wkurts247@gmail.com>
2023-10-10 12:33:29 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -1,15 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from app import app
 
 db = SQLAlchemy()
 
-class Newsletter(db.Model):
-    __tablename__ = 'newsletters'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    body = db.Column(db.String)
-    published_at = db.Column(db.DateTime, server_default=db.func.now())
-    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+ma = Marshmallow(app)
 
-    def __repr__(self):
-        return f'<Newsletter {self.title}, published at {self.published_at}.>'
+
+class NewsletterSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Newsletter
+        load_instance = True
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
+
